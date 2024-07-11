2aaf288dd4286f376b81735f124917cb4bf475df
Thien Nguyen <thie1232135@gmail.com>
2024-07-11 07:30:41 +0700

    fix: fix bugs

--- client/src/classes/Block.ts
+++ client/src/classes/Block.ts
@@ -1,12 +1,16 @@
 import SHA256 from "crypto-js/sha256";
 
 class Block {
-  timestamp: string;
-  transactions: any;
-  previousHash: string;
-  hash: string;
-  nonce: number;
-  validator: string;
+  timestamp: string; // Thời gian tạo khối.
+  transactions: any; // Các giao dịch trong khối.
+  previousHash: string; // Hash của khối trước đó.
+  hash: string; // Hash của khối hiện tại.
+  nonce: number; // Một số ngẫu nhiên được sử dụng trong quá trình đào khối (mining) trong PoW, nhưng trong PoS, 
+  //nó có thể không cần thiết lắm.
+  validator: string; // Người xác nhận (validator) của khối.
+
+  // Hàm khởi tạo này khởi tạo một đối tượng Block với các giá trị ban đầu cho timestamp, transactions, previousHash, và validator. 
+  //Sau đó, nó tính toán giá trị hash cho khối và khởi tạo nonce bằng 0.
 
   constructor(
     timestamp: string,
@@ -21,6 +25,10 @@ class Block {
     this.nonce = 0;
     this.validator = validator;
   }
+
+  //Hàm này tính toán hash của khối bằng cách sử dụng các thuộc tính của khối như 
+  //previousHash, timestamp, transactions, nonce, và validator. 
+  //Kết quả của hàm SHA256 được chuyển đổi thành chuỗi và gán cho thuộc tính hash.
 
   caculateHash() {
     return SHA256(
--- client/src/classes/Transaction.ts
+++ client/src/classes/Transaction.ts
@@ -2,11 +2,13 @@ import SHA256 from "crypto-js/sha256";
 import elliptic from "elliptic";
 
 class Transaction {
-  fromAddress: string | null;
-  toAddress: string | null;
-  amount: number;
-  signature: string;
+  fromAddress: string | null; // Địa chỉ ví gửi tiền, có thể là null (khi giao dịch từ hệ thống).
+  toAddress: string | null; //  Địa chỉ ví nhận tiền.
+  amount: number; // Số tiền giao dịch.
+  signature: string; // Chữ ký của giao dịch.
 
+  // Khởi tạo đối tượng Transaction với các giá trị đầu vào từAddress, toAddress, và amount. signature được khởi tạo là chuỗi rỗng.
+   
   constructor(
     fromAddress: string | null,
     toAddress: string | null,
@@ -18,12 +20,18 @@ class Transaction {
     this.signature = "";
   }
 
+  //Tạo hàm băm cho giao dịch dựa trên các thuộc tính fromAddress, toAddress và amount. Sử dụng SHA256 để tạo ra hàm băm.
+
   caculateHash() {
     if (this.fromAddress && this.toAddress)
       return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
 
     return "";
   }
+
+// Ký giao dịch bằng khóa riêng. Trước hết kiểm tra xem signingKey có khớp với fromAddress không. Nếu không khớp, ném ra một lỗi.
+// Tính hàm băm của giao dịch và ký nó bằng khóa riêng (signingKey).
+// Chữ ký được lưu trữ dưới dạng chuỗi hex.
 
   signTransaction(signingKey: any) {
     if (signingKey.getPublic("hex") !== this.fromAddress) {
@@ -34,6 +42,11 @@ class Transaction {
     const sig = signingKey.sign(hashTx, "base64");
     this.signature = sig.toDER("hex");
   }
+
+// Kiểm tra tính hợp lệ của giao dịch.
+// Nếu fromAddress là null, coi giao dịch là hợp lệ.
+// Nếu không có chữ ký hoặc chữ ký rỗng, ném ra một lỗi.
+// Sử dụng ECC để xác minh chữ ký dựa trên hàm băm của giao dịch và chữ ký lưu trong đối tượng.
 
   isValid() {
     if (this.fromAddress === null) return true;
